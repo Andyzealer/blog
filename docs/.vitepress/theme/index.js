598248c85c1bffb76f5e6930ff24e1d255bcb8f8
Andy <andyzealer@gmail.com>
2023-03-01 14:57:11 +0800

    set default theme

---
+++ docs/.vitepress/theme/index.js
@@ -0,0 +1,22 @@
+// .vitepress/theme/index.js
+import Layout from './Layout.vue'
+import  DefaultTheme  from 'vitepress/theme'
+
+export default {
+  // root component to wrap each page
+  Layout,
+  ...DefaultTheme,
+  // this is a Vue 3 functional component
+  NotFound: () => 'custom 404',
+
+  enhanceApp({ app, router, siteData }) {
+    // app is the Vue 3 app instance from `createApp()`.
+    // router is VitePress' custom router. `siteData` is
+    // a `ref` of current site-level metadata.
+  },
+
+  setup() {
+    // this function will be executed inside VitePressApp's
+    // setup hook. all composition APIs are available here.
+  }
+}
